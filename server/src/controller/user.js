b5b7f6f7d4753f5e622c42cbed785307f21e6679
xds1408@163.com <xian.wu@qiaofang.cn>
2019-10-23 15:23:32 +0800

    基础搭建及登录功能

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,45 +1,30 @@
+/**
+ * user controller
+ */
 const collectionName = 'user'
 const DB = require('../db.js')
-const BASE = require('../base.js')
-const COMMON = require('../common.js')
-const createToken = require('../token/createToken.js')
+// const BASE = require('../base.js')
+const ResultView = require('../result.js')
 
 // 数据库的操作
 
-// 登录
-const login = async (ctx) => {
-  // 拿到账号和密码
-  let username = ctx.request.body.username
-  let password = ctx.request.body.password// sha1(ctx.request.body.password);
-  let result = BASE.Result.getResult()
-  var doc = await DB.find('user', {username, password })
-  doc = doc[0]
-  if (!doc) {
-    console.log('检查到用户名不存在')
-    ctx.status = 200
-    result.status = -1
-    result.message = '用户名不存在'
-    ctx.body = result
-  } else if (doc.password === password) {
-    console.log('密码一致!')
-
-    // 生成一个新的token,并存到数据库
-    let token = createToken(username)
-    console.log(token)
-    ctx.status = 200
-    delete doc.password
-    doc.token = token
-    result.data = doc
-    ctx.body = result
-  } else {
-    console.log('密码错误!')
-    console.log(doc)
-    ctx.status = 200
-    result.status = -1
-    result.message = '密码错误'
-    ctx.body = result
+const getList = async (ctx) => {
+  let body = ctx.request.body || {}
+  let params = {
+    username: body.username || '',
+    sex: body.sex || '',
+    telephone: body.telephone || '',
+    address: body.address || '',
+    email: body.email || ''
   }
+  let doc = await DB.find(collectionName, params) // 过滤没有？？ 位置
+  for (let i = 0; i < doc.length; i++) {
+    delete doc[i].password
+  }
+  let result = (new ResultView()).resultSuccessView(doc)
+  ctx.status = 200
+  ctx.body = result
 }
 module.exports = {
-  login
+  getList
 }
